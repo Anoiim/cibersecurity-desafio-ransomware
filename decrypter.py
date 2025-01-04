46bd247a32e9eb1031911dd1007514a859a264b1
Rychard Alexandre <94056198+Anoiim@users.noreply.github.com>
2025-01-03 22:15:42 -0300

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,29 @@
 import os
 import pyaes
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+file_name = 'teste.txt.ransomwaretroll'
+if os.path.exists(file_name):
+    try:
+        # Abrir arquivo
+        file = open(file_name, 'rb')
+        file_data = file.read()
+        file.close()
 
-## chave para descriptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
+        # Chave para descriptografar
+        key = b'testeransomware1'
+        aes = pyaes.AESModeOfOperationCTR(key)
+        decrypt_data = aes.decrypt(file_data)
 
-## remover o arquivo criptografado
-os.remove(file_name)
+        # Remover arquivo
+        os.remove(file_name)
 
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
-new_file.close()
+        # Gerar novo arquivo
+        new_file = 'teste.txt'
+        with open(new_file, 'wb') as new_file:
+            new_file.write(decrypt_data)
+
+        print("arquivo criptografado")
+    except Exception as e:
+        print(f"Erro ao processar o arquivo: {e}")
+else:
+    print(f"Arquivo {file_name} não encontrado.")
